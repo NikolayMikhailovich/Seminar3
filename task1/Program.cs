5a21541c8589d471bf837f9f054ff8863f354259
nikolays <sidorovkolya91@gmail.com>
2023-01-20 20:31:47 +0300

    1задача

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// // Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 или Y ≠ 0 и выдаёт номер четверти плоскости, 
+// в которой находится эта точка.
+
+int PromptInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return int.Parse(Console.ReadLine());
+}
+
+bool ValidateCoords(int xCoord, int yCoord)
+{
+    if (xCoord == 0 || yCoord == 0)
+    {
+        System.Console.WriteLine("Хотя бы одна координата находится на оси.");
+        return false;
+    }
+    return true;
+}
+
+int GetQuorter(int xCoord, int yCoord)
+{
+    if (xCoord > 0 && yCoord > 0)
+    {
+        return 1;
+    }
+    if (xCoord > 0 && yCoord < 0)
+    {
+        return 4;
+    }
+    if (xCoord < 0 && yCoord > 0)
+    {
+        return 2;
+    }
+    return 3;
+}
+
+int x = PromptInt("Введите X");
+int y = PromptInt("Введите Y");
+if (ValidateCoords(x, y))
+{
+    int quorter = GetQuorter(x, y);
+    System.Console.WriteLine($"Номер четверти {quorter}");
+}
